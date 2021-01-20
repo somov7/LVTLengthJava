708b6b857bb6432b7eabce05e3b7da31234deabe
Artyom Somov <avsomov7@gmail.com>
2021-01-20 23:59:56 +0300

    Initial commit

---
+++ src/main/java/LVT.java
@@ -0,0 +1,33 @@
+import java.math.BigInteger;
+
+public class LVT {
+    static public byte[] encodeLength(BigInteger value) {
+        assert (value.compareTo(BigInteger.ZERO) > 0);
+        assert (value.bitLength() <= 1008);
+        if (value.compareTo(BigInteger.valueOf(127)) <= 0) {
+            byte[] encoded = new byte[1];
+            encoded[0] = (byte) (value.intValue() & 0x7F);
+            return encoded;
+        }
+        byte[] encoded = new byte[(value.bitLength() + 7) / 8 + 1];
+        encoded[0] = (byte) ((1 << 7) | ((encoded.length - 1) & 0x7F));
+        for (int i = encoded.length - 1; i >= 1; i--) {
+            BigInteger[] qr = value.divideAndRemainder(BigInteger.valueOf(256));
+            encoded[i] = (byte) (qr[1].intValue() & 0xFF);
+            value = qr[0];
+        }
+        return encoded;
+    }
+
+    static public BigInteger decodeLength(byte[] value) {
+        boolean isShortForm = (value[0] & (1 << 7)) == 0;
+        if (isShortForm) {
+            return BigInteger.valueOf(value[0] & 0x7F);
+        }
+        int length = value[0] & 0x7F;
+        value[0] = 0;
+        BigInteger decoded = new BigInteger(1, value);
+        value[0] = (byte) ((1 << 7) | length);
+        return decoded;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            throw new Exception("Expected exactly 2 arguments");
+        }
+        if (args[0].equals("e")) {
+            BigInteger value = new BigInteger(args[1]);
+            byte[] encoded = LVT.encodeLength(value);
+            StringBuilder encodedString = new StringBuilder();
+            for (byte digit: encoded) {
+                encodedString.append(String.format("%02X", digit));
+            }
+            System.out.println(encodedString);
+        }
+        else if(args[0].equals("d")) {
+            byte[] encoded = (new BigInteger(args[1], 16)).toByteArray();
+            byte[] tmp = new byte[encoded.length - 1];
+            System.arraycopy(encoded, 1, tmp, 0, tmp.length);
+            encoded = tmp;
+            BigInteger value = LVT.decodeLength(encoded);
+            System.out.println(value);
+        }
+    }
+}
